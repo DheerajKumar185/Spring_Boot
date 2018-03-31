f220ed380ffdc9b4e8fcbc5479d2200e65bf3dd7
DOK$ <DOK$@DOK-PC>
2018-03-31 14:53:40 +0530

    Initial Version with derby DB.

---
+++ hello_boot_data/src/main/java/com/tech/spring/boot/course/CourseService.java
@@ -0,0 +1,57 @@
+package com.tech.spring.boot.course;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CourseService {
+	
+	@Autowired
+	private CourseRepository courseRepository;
+	
+	/*List<Course> topics = new ArrayList<>(Arrays.asList(
+			new Course("Java", "Java 8", "Lambda expression"),
+			new Course("Spring", "Spring Framework", "Spring Framework Description"),
+			new Course("Hibernamte", "Hibernate Framework", "Hibernate Framework Description")
+			));*/
+	
+	public List<Course> getCourses(String topicId) {
+		/*return topics;*/
+		List<Course> courses = new ArrayList<>();
+		courseRepository.findByTopicId(topicId)
+				.forEach(courses::add);
+		return courses;
+	}
+	
+	public Course getCourse(String id) {
+		/*return topics.stream().filter(t->t.getId().equals(id)).findFirst().get();*/
+		return courseRepository.findOne(id);
+	}
+	
+	public void addCourse(Course course) {
+		/*topics.add(topic);*/
+		courseRepository.save(course);
+	}
+
+	public void updateCourse(Course course) {
+		/*for(int i=0; i< topics.size(); i++) {
+			Topic t = topics.get(i);
+			if(t.getId().equals(id)) {
+				topics.set(i, topic);
+			}
+		}*/
+		
+		courseRepository.save(course);
+		
+	}
+
+	public void deleteCourse(String id) {
+		/*topics.removeIf(t->t.getId().equals(id));*/
+		courseRepository.delete(id);
+		
+	}
+
+}
